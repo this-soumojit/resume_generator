0fbf396a75d80b20bd8d895bd186d12e0de1e00c
Soumojit Ghosh <soumojitghosh325@gmail.com>
2023-11-17 02:14:26 +0530

    added moving text

--- script.js
+++ script.js
@@ -1,56 +1,59 @@
 // alert("loading")
-function addForm(){
-    window.location.href = "main.html"
+// function addForm(){
+//     window.location.href = "main.html"
+// }
+function redirectToGenerator() {
+    windows.location.href = "main.html"
 }
 
 
-function addNewWefield(){
+function addNewWefield() {
     // console.log("Adding new field");
 
-    let newNode=document.createElement("textarea");
+    let newNode = document.createElement("textarea");
     newNode.classList.add("form-control");
     newNode.classList.add("weField");
     newNode.classList.add("mt-2");
-    newNode.setAttribute("rows",3);
-    newNode.setAttribute("Placeholder","Enter here");
-    
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("Placeholder", "Enter here");
+
 
     let weOb = document.getElementById("we");
     let weAddButtonOb = document.getElementById("weAddButton");
 
-    weOb.insertBefore(newNode,weAddButtonOb);
+    weOb.insertBefore(newNode, weAddButtonOb);
 }
 
-function addNewAqField(){
+function addNewAqField() {
 
-    let newNode=document.createElement("textarea");
+    let newNode = document.createElement("textarea");
     newNode.classList.add("form-control");
     newNode.classList.add("aqField");
     newNode.classList.add("mt-2");
     // newNode.setAttribute("rows",3);
-    newNode.setAttribute("placeholder","Enter here");
+    newNode.setAttribute("placeholder", "Enter here");
 
     let aqOb = document.getElementById("aq");
     let aqAddButtonOb = document.getElementById("aqAddButton");
 
-    aqOb.insertBefore(newNode,aqAddButtonOb);
+    aqOb.insertBefore(newNode, aqAddButtonOb);
 }
 
 // generate CV
 
-function generateCV(){
+function generateCV() {
     // console.log("generating Cv");
 
-    let nameField=document.getElementById("nameField").value;
-    let nameTemplate=document.getElementById("nameTemplate");
+    let nameField = document.getElementById("nameField").value;
+    let nameTemplate = document.getElementById("nameTemplate");
 
     nameTemplate.innerHTML = nameField;
 
-    document.getElementById("nameTemplate2").innerHTML=nameField;
+    document.getElementById("nameTemplate2").innerHTML = nameField;
 
-    document.getElementById("contactTemplate").innerHTML=document.getElementById("contactField").value;
+    document.getElementById("contactTemplate").innerHTML = document.getElementById("contactField").value;
 
-    document.getElementById("emailTemplate").innerHTML=document.getElementById("emailField").value;
+    document.getElementById("emailTemplate").innerHTML = document.getElementById("emailField").value;
 
     var input = document.getElementById('imageField');
     var preview = document.getElementById('proTemp');
@@ -67,9 +70,9 @@ function generateCV(){
         reader.readAsDataURL(file);
     }
 
-    document.getElementById("birthTemplate").innerHTML=document.getElementById("birthField").value;
+    document.getElementById("birthTemplate").innerHTML = document.getElementById("birthField").value;
 
-    document.getElementById("addressTemplate").innerHTML=document.getElementById("addressField").value;
+    document.getElementById("addressTemplate").innerHTML = document.getElementById("addressField").value;
 
     var linkedField = document.getElementById('LinkedField').value;
     var lidynamicLink = document.getElementById('lidynamicLink');
@@ -78,7 +81,7 @@ function generateCV(){
     lidynamicLink.href = linkedField;
 
 
-    document.getElementById('fbdynamicLink').href=document.getElementById('fbField').value;
+    document.getElementById('fbdynamicLink').href = document.getElementById('fbField').value;
 
 
 
@@ -89,42 +92,41 @@ function generateCV(){
 
     // document.getElementById("linkedTemplate").innerHTML=document.getElementById("LinkedField").value;
 
-    
-    document.getElementById("objTemplate").innerHTML=document.getElementById("objectiveField").value;
+
+    document.getElementById("objTemplate").innerHTML = document.getElementById("objectiveField").value;
 
     // work experiences
 
-    let wes=document.getElementsByClassName("weField");
+    let wes = document.getElementsByClassName("weField");
 
-    let str=""
+    let str = ""
 
-    for(let e of wes){
+    for (let e of wes) {
         str = str + `<li> ${e.value}</li>`;
     }
 
-    document.getElementById("weTemplate").innerHTML= str;
+    document.getElementById("weTemplate").innerHTML = str;
 
     // academic qualification
 
-    let aqs=document.getElementsByClassName("aqField");
+    let aqs = document.getElementsByClassName("aqField");
 
-    let str1="";
+    let str1 = "";
 
-    for(let e of aqs)
-    {
+    for (let e of aqs) {
         str1 = str1 + `<li> ${e.value}</li>`;
     }
 
     document.getElementById("aqTemplate").innerHTML = str1;
-    
 
-    document.getElementById("cv-form").style.display="none";
-    document.getElementById("cv-template").style.display="block";
+
+    document.getElementById("cv-form").style.display = "none";
+    document.getElementById("cv-template").style.display = "block";
 
 
 
 }
 
-function printCV(){
+function printCV() {
     window.print();
 }
